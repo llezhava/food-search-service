dc117af806426decb99f43784f09e10b319c047f
Lasha Lezhava <lashalezhava@gmail.com>
2024-04-24 20:49:27 +0400

    feat: add seed data

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+
+import brandNames from './data/brands';
+import cityNames from './data/cities';
+import dietNames from './data/diets';
+import dishTypeNames from './data/cities';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  const brands = await prisma.brand.createMany({
+    data: brandNames.map((name) => ({ name })),
+  });
+
+  const cities = await prisma.city.createMany({
+    data: cityNames.map((name) => ({ name })),
+  });
+
+  const diets = await prisma.diet.createMany({
+    data: dietNames.map((name) => ({ name })),
+  });
+
+  const dishTypes = await prisma.dishType.createMany({
+    data: dishTypeNames.map((name) => ({ name })),
+  });
+
+  console.log('Seeding data results:', { brands, cities, diets, dishTypes });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
